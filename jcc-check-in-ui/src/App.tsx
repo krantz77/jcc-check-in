5c5766b8b664644438b98cd82dffeb6f4e542ba0
Kelsey Krantz <kelsey.krantz@xandr.com>
2023-01-09 13:28:57 -0500

    old version

--- jcc-check-in-ui/src/App.tsx
+++ jcc-check-in-ui/src/App.tsx
@@ -1,24 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.png';
 import './App.css';
 import MyDropzone, {IDropzoneProps} from "./Components/MyDropzone";
-import { OutTable } from "react-excel-renderer";
 import _ from "lodash"
-import {formatData} from "./Utils/DataTable.util";
+import {checkIn, formatData} from "./Utils/DataTable.util";
 import {IRow} from "./Interfaces/interfaces";
-import set = Reflect.set;
 import DataTable from "lucid-ui/dist/esm/components/DataTable/DataTable";
 
 function App() {
-    //const [data, setData] = useState([] as IRow[]);
-    // const [data, setData] = useState({} as{rows:[], cols:[]});
-    const [data, setData] = useState({rows: [], cols: []} as any )
+    const [data, setData] = useState({rows: [] as [], cols: []} as any );
+    const [dataFormatted, setDataFormatted] = useState([] as IRow[])
+    const [showButtons, setShowButtons] = useState(false);
+    const [selected, setSelected]= useState(-1);
     var key = 0;
 
+    // const columnNames = data.rows? _.map(data.rows[0], (col) => {
+    //     return {column: col, key: key+1}}) : data.cols;
+    const selectColumn= (index: IRow) => {
+        console.log(index)
+        setSelected(index.id);
+        setShowButtons(!showButtons)
+    }
     console.log(data)
-    const columnNames = data.rows? _.map(data.rows[0], (col) => {
-        return {column: col, key: key+1}}) : data.cols;
-    console.log(columnNames)
 
     return (
     <div className="App">
@@ -28,15 +31,22 @@ function App() {
         JCC Krakow Check In
         </div>
       </header>
+        <div className="Grid-Header">
+            {showButtons &&
+            <button onClick={() => checkIn(selected, data.rows as IRow[], setData as (data: {}) => {})}>
+                check in
+            </button>}
+        </div>
         <div className="App-Body">
             <p>
-                {/*<MyDropzone set={(data: any) => setData(formatData(data)) as unknown as (resp:any) => {}}/>*/}
-                {/*<MyDropzone set={(data) => formatData(setData as (data:IRow[]) => {}, data) } />*/}
                 <MyDropzone set={setData}/>
                 <div  className="data-table">
-
                 {data ? (
-                    <DataTable data={formatData(data.rows)}>
+                    <DataTable
+                        data={formatData(data.rows? data.rows: data, )}
+                        isSelectable
+                        onSelect={(index) => selectColumn(index)}
+                    >
                         {
                             _.map(data.rows[0], (col: string) => {
                             return (
--- jcc-check-in-ui/src/Utils/DataTable.util.tsx
+++ jcc-check-in-ui/src/Utils/DataTable.util.tsx
@@ -2,21 +2,15 @@ import {IRow} from "../Interfaces/interfaces"
 import _ from "lodash";
 
 const checkLastVisit = (date: string) => {
-    console.log(date)
     console.log(new Date())
     const today = new Date().getTime();
     const lastVisit = Date.parse(date)
     const difference = (today - lastVisit)/(1000*3600*24);
-    console.log(Date.parse(date))
-    console.log(lastVisit)
-    console.log(today)
-    console.log(difference)
     return difference <= 7 ? 'Yes' : 'No'
 }
-export const formatData = (rows: any, setData?: (data: IRow[]) => {},) => {
+export const formatData = (rows: any, setDataFormatted?: any) => {
     console.log(rows)
         const data: IRow[] = _.map(rows, (row) => {
-            console.log(row[2] ? checkLastVisit(row[2]) : 'No')
             return {
                 id: row[0] ? row[0] : -1,
                 householdMembers: row[1] ? row[1] : [],
@@ -26,8 +20,29 @@ export const formatData = (rows: any, setData?: (data: IRow[]) => {},) => {
                 hometown: row[5] ? row[5] : 'unknown'
             }
         })
-
-        console.log(data)
-        //setData(data);
+    // if (setDataFormatted && !_.isEmpty(data)) {
+    //     setDataFormatted({rows:_.drop(data)})
+    // }
+    console.log(data)
         return _.drop(data)
 };
+
+export const checkIn = (id: number, data: any, setData: (data: { rows: unknown[] }) => {})=> {
+    console.log(data)
+   const result =_.map(data, (row) => {
+       console.log(row)
+        if(row[0] == id) {
+            console.log('here')
+            return {
+                id: row[0],
+                householdMembers:[row[1]],
+                lastVisit: new Date().toString(),
+                visitedInLastWeek: 'Yes',
+            }
+        }
+        return row
+    })
+    console.log(result)
+    setData({rows: result})
+}
+
